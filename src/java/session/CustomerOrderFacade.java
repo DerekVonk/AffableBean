5a9ed2d77da8444ad1276727bcaaa6ca3734c1ee
Derek Vonk <derek.vonk@hva.nl>
2015-11-18 22:22:00 +0100

    add session beans

---
+++ src/java/session/CustomerOrderFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import entity.CustomerOrder;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author vonk
+ */
+@Stateless
+public class CustomerOrderFacade extends AbstractFacade<CustomerOrder> {
+    @PersistenceContext(unitName = "AffableBeanPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public CustomerOrderFacade() {
+        super(CustomerOrder.class);
+    }
+    
+}
