cf9fe1fec3353eeee2bed41e37372f26f21d0271
Derek Vonk <derek.vonk@hva.nl>
2015-11-24 17:15:48 +0100

    setting up the Transaction Programmatically

--- src/java/session/OrderManager.java
+++ src/java/session/OrderManager.java
@@ -15,6 +15,10 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Random;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -23,16 +27,22 @@ import javax.persistence.PersistenceContext;
  * @author Vonk
  */
 @Stateless
+@TransactionManagement(TransactionManagementType.CONTAINER) //default setting, implemented for readability
 public class OrderManager {
-    
+
     @PersistenceContext(unitName = "AffableBeanPU")
     private EntityManager em;
 
+    @TransactionAttribute(TransactionAttributeType.REQUIRED) //default setting, implemented for readability
     public int placeOrder(String name, String email, String phone, String address, String cityRegion, String ccNumber, ShoppingCart cart) {
-        Customer customer = addCustomer(name, email, phone, address, cityRegion, ccNumber);
-        CustomerOrder order = addOrder(customer, cart);
-        addOrderedItems(order, cart);
-        return order.getId();
+        try {
+            Customer customer = addCustomer(name, email, phone, address, cityRegion, ccNumber);
+            CustomerOrder order = addOrder(customer, cart);
+            addOrderedItems(order, cart);
+            return order.getId();
+        } catch (Exception e) {
+            return '0';
+        }
     }
 
     private Customer addCustomer(String name, String email, String phone, String address, String cityRegion, String ccNumber) {
@@ -43,62 +53,62 @@ public class OrderManager {
         customer.setAddress(address);
         customer.setCityRegion(cityRegion);
         customer.setCcNumber(ccNumber);
-        
+
         // to wright to the database via the EntityManager API
         em.persist(customer);
-        
+
         return customer;
     }
 
     private CustomerOrder addOrder(Customer customer, ShoppingCart cart) {
-        
+
         //setup customer order
         CustomerOrder order = new CustomerOrder();
         order.setCustomer(customer);
         order.setAmount(BigDecimal.valueOf(cart.getTotal()));
-        
+
         //create confirmation order number
         Random random = new Random();
         int i = random.nextInt(999999999);
         order.setConfirmationNumber(i);
-        
+
         // to wright to the database via the EntityManager API
         em.persist(order);
-        
+
         return order;
     }
 
     private void addOrderedItems(CustomerOrder order, ShoppingCart cart) {
-        
+
 //        Since the ID is an auto-incrementing primary key, the database 
 //        automatically generates the value only when the record is added. 
 //        One way to overcome this is to manually synchronize the persistence 
 //        context with the database. 
 //        This can be accomplished using the EntityManager's flush method.
         em.flush();
-        
+
         List<ShoppingCartItem> items = cart.getItems();
-        
+
         //iterate through ShoppingCartItem list and create orderedProducts
         for (ShoppingCartItem scItem : items) {
-            
+
             int productId = scItem.getProduct().getId();
-            
+
             // set up primary key object
             OrderedProductPK orderedProductPK = new OrderedProductPK();
             orderedProductPK.setCustomerOrderId(order.getId());
             orderedProductPK.setProductId(productId);
-            
+
             // create ordered item using PK Object
             OrderedProduct orderedItem = new OrderedProduct(orderedProductPK);
-            
+
             // set quantity
             orderedItem.setQuantity(scItem.getQuantity());
-            
+
             // wright to the database via the EntityManager API
             em.persist(orderedItem);
         }
-        
+
     }
 
 }
